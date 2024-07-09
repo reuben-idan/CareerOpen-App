80d494d3e7d87c4206898f6d4e97881a1ffb787e
reuben-idan <nobleadonis2@gmail>
2024-07-09 02:26:56 +0000

    Restructure User flow

--- src/App.js
+++ src/App.js
@@ -1,46 +1,43 @@
-import React, { useState } from 'react';
-import Registration from './components/Registration';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Container, Navbar, Nav } from 'react-bootstrap';
 import Login from './components/Login';
+import Registration from './components/Registration';
 import JobSeekerProfile from './components/JobSeekerProfile';
 import EmployerProfile from './components/EmployerProfile';
 
-
-
-
 const App = () => {
-  const [user, setUser] = useState(null);
-  const [currentStep, setCurrentStep] = useState('registration');
-
-  const handleRegistration = (userData) => {
-    // Simulate user registration
-    setUser({ ...userData, loggedIn: false });
-    setCurrentStep('login');
-  };
-
-  const handleLogin = (loginData) => {
-    // Simulate user login
-    setUser({ ...user, loggedIn: true });
-    setCurrentStep('profile');
-  };
-
-  const handleLogout = () => {
-    setUser(null);
-    setCurrentStep('registration');
-  };
-
   return (
-    <div className="app-container">
-      {currentStep === 'registration' && (
-        <Registration onSubmit={handleRegistration} />
-      )}
-      {currentStep === 'login' && <Login onLogin={handleLogin} />}
-      {currentStep === 'profile' && user.userType === 'jobSeeker' && (
-        <JobSeekerProfile onLogout={handleLogout} />
-      )}
-      {currentStep === 'profile' && user.userType === 'employer' && (
-        <EmployerProfile onLogout={handleLogout} />
-      )}
-    </div>
+    <Router>
+      <div>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand as={Link} to="/">
+              CareerOpen
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link as={Link} to="/login">
+                  Login
+                </Nav.Link>
+                <Nav.Link as={Link} to="/registration">
+                Sign Up
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/jobseeker-profile" element={<JobSeekerProfile />} />
+          <Route path="/employer-profile" element={<EmployerProfile />} />
+          {/* Add other routes as needed */}
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
