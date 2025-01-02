a775ae4eb621909edfe0abf77d13e4a03e1776c6
reuben-idan <nobeadonis2@gmail.com>
2025-01-02 20:56:55 +0000

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Ensure this matches Vercel's expected directory
+  },
+});
+
