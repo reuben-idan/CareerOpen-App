8738f974587f0858e474cd99c49e463d960b6ba9
reuben-idan <nobleadonis2@gmail>
2025-08-12 23:24:41 +0000

    fix: add TEMPLATES setting to prod_settings.py to include templates directory

--- careeropen-backend/core/prod_settings.py
+++ careeropen-backend/core/prod_settings.py
@@ -120,6 +120,23 @@ DATABASES = {
     'default': get_database_config()
 }
 
+# Template configuration
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 # Connection pooling for production
 DATABASE_POOL_ARGS = {
     'max_overflow': 10,
