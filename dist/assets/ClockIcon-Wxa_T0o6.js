1521f91ef4f5487befd34de9ef484580dbfbeab6
User <user@example.com>
2025-08-14 05:59:27 +0000

    Fix Vite configuration and update build process

---
+++ dist/assets/ClockIcon-Wxa_T0o6.js
@@ -0,0 +1,2 @@
+import{r as e}from"./index-CAXHRMTp.js";function a({title:r,titleId:o,...t},n){return e.createElement("svg",Object.assign({xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24",strokeWidth:1.5,stroke:"currentColor","aria-hidden":"true","data-slot":"icon",ref:n,"aria-labelledby":o},t),r?e.createElement("title",{id:o},r):null,e.createElement("path",{strokeLinecap:"round",strokeLinejoin:"round",d:"M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"}))}const i=e.forwardRef(a);export{i as F};
+//# sourceMappingURL=ClockIcon-Wxa_T0o6.js.map
